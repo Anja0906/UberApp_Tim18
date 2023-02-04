bf1e2ae2813a0ac6ace7abcea05c0bafe6378ee1
Anja Petković <anjapetkovic92@gmail.com>
2023-02-04 18:21:05 +0100

    GUI completed

--- UberApp_Tim18/app/src/main/java/com/example/uberapp_tim18/fragments/FavoriteRides.java
+++ UberApp_Tim18/app/src/main/java/com/example/uberapp_tim18/fragments/FavoriteRides.java
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.example.uberapp_tim18.Adapters.FavoriteRoutesAdapter;
 import com.example.uberapp_tim18.Adapters.ReportsAdapter;
@@ -20,6 +22,13 @@ import DTO.FavoriteRoute;
 import DTO.ReportItem;
 
 public class FavoriteRides extends Fragment {
+    View rootView;
+    ListView listView;
+    ImageView add;
+    ImageView confirm;
+    ImageView cancel;
+    RelativeLayout list;
+    RelativeLayout addFav;
 
     public FavoriteRides() {
     }
@@ -32,8 +41,46 @@ public class FavoriteRides extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_favorite_rides, container, false);
+        rootView = inflater.inflate(R.layout.fragment_favorite_rides, container, false);
+        initList();
+        initGUI();
+        return rootView;
+    }
+
+    private void initGUI() {
+        list = rootView.findViewById(R.id.list_layout);
+        addFav = rootView.findViewById(R.id.form);
+        add = rootView.findViewById(R.id.add_favorite_ride);
+        confirm = rootView.findViewById(R.id.confirm_button);
+        cancel = rootView.findViewById(R.id.cancel_edit_data);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                list.setVisibility(View.GONE);
+                addFav.setVisibility(View.VISIBLE);
+            }
+        });
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                list.setVisibility(View.VISIBLE);
+                addFav.setVisibility(View.GONE);
+                //dodati u listu svih
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                list.setVisibility(View.VISIBLE);
+                addFav.setVisibility(View.GONE);
+                //samo odbaciti promene
+            }
+        });
+
+    }
+
+    private void initList(){
         List<FavoriteRoute> items = new ArrayList<>();
         items.add(new FavoriteRoute("huwdakusfhuahfu", "ajfjkhdaskjhfdahjasfj"));
         items.add(new FavoriteRoute("ksdhfahfa", "akshcduiascn"));
@@ -41,8 +88,7 @@ public class FavoriteRides extends Fragment {
         items.add(new FavoriteRoute("jzcbjbsjakcsaljcscks", ""));
         items.add(new FavoriteRoute("huwdakusfhuscjjkshacahfu", "ajkshcxkhaskjchjsakcklsajdksa"));
         FavoriteRoutesAdapter adapter = new FavoriteRoutesAdapter(this.getContext(), items);
-        ListView listView = rootView.findViewById(R.id.list_view);
+        listView = rootView.findViewById(R.id.list_view);
         listView.setAdapter(adapter);
-        return rootView;
     }
 }
